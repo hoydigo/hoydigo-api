b01228ed07b76124c0588a6babd5685010cb2ec6
Abel Moreno <moreno.abel@gmail.com>
2021-04-27 19:38:58 -0500

    Adding swagger for OpenAPI spec

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(title="hoydigo API", version="1.0.0")
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     name="Bearer Token",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     securityScheme="bearer",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
--- src/app/Http/Controllers/api/v1/AuthController.php
+++ src/app/Http/Controllers/api/v1/AuthController.php
@@ -17,6 +17,39 @@ class AuthController extends Controller
      * @param Request $request
      *
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     *
+     * @OA\Post(
+     *     path="/api/v1/user/get_token",
+     *     tags={"Auth"},
+     *     summary="Get new access token",
+     *     description="Get new access token",
+     *     operationId="getToken",
+     *     @OA\RequestBody(
+     *      required=true,
+     *      description="Pass user credentials",
+     *      @OA\MediaType(
+     *          mediaType="multipart/form-data",
+     *          @OA\Schema(
+     *              @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+     *              @OA\Property(property="password", type="password", format="password", example="1234567"),
+     *          ),
+     *      ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="New token returned",
+     *         @OA\JsonContent(
+     *              required={"email","password"},
+     *              @OA\Property(property="name", type="string", format="text", example="User Name"),
+     *              @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+     *              @OA\Property(property="access_token", type="string", format="text", example="NEW_ACESS_TOKEN"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Invalid login credentials"
+     *     )
+     * )
      */
     public function getToken(Request $request): JsonResponse
     {
@@ -50,6 +83,50 @@ class AuthController extends Controller
      * @param Request $request
      *
      * @return JsonResponse
+     *
+     * @OA\Post(
+     *     path="/api/v1/user/register",
+     *     tags={"Auth"},
+     *     summary="Register a new user",
+     *     description="Create a new user in the app",
+     *     operationId="registerUser",
+     *     security={{ "bearer": {} }},
+     *     @OA\RequestBody(
+     *      required=true,
+     *      description="Fields dor the new user",
+     *      @OA\MediaType(
+     *          mediaType="multipart/form-data",
+     *          @OA\Schema(
+     *              @OA\Property(property="name", type="string", format="text", example="User Name"),
+     *              @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+     *              @OA\Property(property="password", type="password", format="password", example="1234567"),
+     *              @OA\Property(property="role", type="string", format="text", example="web-guest"),
+     *          ),
+     *      ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="New user registered",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", format="text", example="User registered successfully"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", format="text", example="Unauthenticated"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad Request",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="field1", type="string", format="text", example="Error description"),
+     *              @OA\Property(property="field2", type="string", format="text", example="Error description"),
+     *         ),
+     *     )
+     * )
      */
     public function register(Request $request): JsonResponse
     {
